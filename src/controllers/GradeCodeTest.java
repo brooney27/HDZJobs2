a79c60fb60cba8fafa6a8077c02a2a245b30205b
brooney27 <brooney278@gmail.com>
2016-07-29 11:23:28 -0400

    preliminary codetest

--- src/controllers/GradeCodeTest.java
+++ src/controllers/GradeCodeTest.java
@@ -1,11 +1,16 @@
 package controllers;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.HdzApplication;
+import services.InterviewService;
 
 /**
  * Servlet implementation class GradeCodeTest
@@ -26,8 +31,35 @@ public class GradeCodeTest extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int questions=0;
+		int numCorrect=0;
+		int questionid = 1;
+		
+		String appid = request.getParameter("appid");
+		
+		HdzApplication application = InterviewService.getHdzApplication(appid);
+		
+		while(questions<10){
+			String useranswer = request.getParameter("response"+questionid);
+			String answer = request.getParameter("answer"+questionid);
+			if(useranswer!=null){
+				questions++;
+				if(useranswer.equals(answer))numCorrect++;
+			}
+			questionid++;
+		}
+		
+		application.setCodingtestscore(new BigDecimal(numCorrect));
+		if(numCorrect>6){
+			application.setCodingtest("P");
+		}
+		else{
+			application.setCodingtest("F");
+			application.setAppstatus("Fail");
+		}
+		InterviewService.updateApplication(application);
+		
+		request.getRequestDispatcher("/PendingActions").forward(request, response);
 	}
 
 	/**
