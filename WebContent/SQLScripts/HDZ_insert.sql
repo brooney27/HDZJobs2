bf67f160912a8a25ac9014fa3dc705034f89f3e3
sanjukthas <sanjuktha23@gmail.com>
2016-08-01 14:50:21 -0400

    pull

--- WebContent/SQLScripts/HDZ_insert.sql
+++ WebContent/SQLScripts/HDZ_insert.sql
@@ -147,6 +147,9 @@ values (hdz_jobskillbridge_id_seq.nextval,3,3,'2');
 
 insert into HDZ_jobskillbridge (jobskillbridgeid, jobsid,skillsid,experience)
 values (hdz_jobskillbridge_id_seq.nextval,3,7,'2');
+
+insert into HDZ_jobskillbridge (jobskillbridgeid, jobsid,skillsid,experience)
+values (hdz_jobskillbridge_id_seq.nextval,4,8,'2');
 --application
 
 insert into HDZ_application (applicationid, applicantID,codingtest, jobsID,appstatus,comments,appscore,codingtestscore)
